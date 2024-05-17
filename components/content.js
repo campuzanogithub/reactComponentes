17278cbf6bd9c88768515d21ffb82395fcd86172
campuzanogithub <128656913+campuzanogithub@users.noreply.github.com>
2024-05-16 19:09:25 -0500

    Add files via upload

---
+++ components/content.js
@@ -0,0 +1,10 @@
+import Articles from "./articles";
+function Content (){
+    return(
+        <div className="col-sm-8">
+            <Articles />
+        </div>
+    );
+}
+
+export default Content
---
+++ components/footer.js
@@ -0,0 +1,10 @@
+
+function Footer (){
+    return(
+        <div className="mt-5 p-4 bg-dark text-white text-center">
+            <p>Footer - By Santiago campuzano- All rights reserved</p>
+        </div>
+    );
+}
+
+export default Footer;
